ee6396b65f6b4e0e39c6287216ea50d41e0e12c4
jdrdo <jader.raoliveira@gmail.com>
2026-04-13 15:22:46 -0300

    adicionando numeracao nas barras para relatorio mais completo e preciso no grafico

--- filtragem de relatorio de entregas.py
+++ filtragem de relatorio de entregas.py
@@ -38,16 +38,30 @@ conn = sqlite3.connect('logistica_pessoal.db') #conexao com o banco de dados
 #conn = sqlite3.connect(r'C:\Users\Jader\Documents\logistica_pessoal.db')
 df.to_sql('entregas', conn, if_exists='replace', index=False) #aqui eu digo que a tabela entregas existe e que se existe refazer
 
+#query_top_dia = """
+#SELECT data_entregas, bairro, MAX(lucro_total) as lucro_maximo
+#FROM (
+    #SELECT data_entregas, bairro, SUM(taxa) AS lucro_total
+   # SELECT lucro_maximo, COUNT(lucro_total) as quantidade_entregas
+   # FROM entregas
+   # GROUP BY data_entregas, bairro
+#)
+#GROUP BY data_entregas;
+#"""
 query_top_dia = """
-SELECT data_entregas, bairro, MAX(lucro_total) as lucro_maximo
+SELECT data_entregas, bairro, lucro_maximo, quantidade_entregas
 FROM (
-    SELECT data_entregas, bairro, SUM(taxa) AS lucro_total
+    SELECT 
+        data_entregas, 
+        bairro, 
+        SUM(taxa) AS lucro_maximo,
+        COUNT(*) AS quantidade_entregas,
+        ROW_NUMBER() OVER(PARTITION BY data_entregas ORDER BY SUM(taxa) DESC) as rank
     FROM entregas
     GROUP BY data_entregas, bairro
 )
-GROUP BY data_entregas;
+WHERE rank = 1;
 """
-
 query_volume_bairro = """
 SELECT 
     bairro, 
@@ -58,22 +72,24 @@ ORDER BY quantidade_entregas DESC;
 """
 
 
-
-# 1. Busca os dados do campeão de cada dia
 df_top = pd.read_sql(query_top_dia, conn)
 df_volume = pd.read_sql(query_volume_bairro, conn)
 conn.close()
-# 2. Criando o gráfico
+
+total_entregas_bairro = df_top['quantidade_entregas'].sum()
 plt.figure(figsize=(12, 6))
 bars = plt.bar(df_top['data_entregas'], df_top['lucro_maximo'], color='skyblue')
 
-# 3. A MÁGICA: Colocar o nome do bairro em cima da barra
-for bar, bairro in zip(bars, df_top['bairro']):
+#for bar, bairro in zip(bars, df_top['bairro']):
+    #yval = bar.get_height()
+    #plt.text(bar.get_x() + bar.get_width()/2, yval + 0.2, bairro, 
+             #ha='center', va='bottom', fontweight='bold', color='darkblue')
+for bar, bairro, qtd in zip(bars, df_top['bairro'], df_top['lucro_maximo']):
     yval = bar.get_height()
-    plt.text(bar.get_x() + bar.get_width()/2, yval + 0.2, bairro, 
-             ha='center', va='bottom', fontweight='bold', color='darkblue')
+    plt.text(bar.get_x() + bar.get_width()/2, yval + 0.5, 
+             f'{bairro}\n({int(qtd)})', 
+             ha='center', va='bottom', fontweight='bold', color='darkblue', fontsize=9)
 
-# Perfumaria
 plt.title('Bairro Mais Lucrativo por Dia (Top Performance)', fontsize=14)
 plt.xlabel('Data')
 plt.ylabel('Lucro do Melhor Bairro (R$)')
@@ -86,11 +102,20 @@ plt.show()
 
 
 
-# --- Gráfico de Quantidade de Entregas ---
+total_entregas = df_volume['quantidade_entregas'].sum()
 plt.figure(figsize=(10, 6))
 # Usando gráfico de barras horizontais (barh) para facilitar a leitura dos nomes
-plt.barh(df_volume['bairro'], df_volume['quantidade_entregas'], color='salmon')
-
+bars_a = plt.barh(df_volume['bairro'], df_volume['quantidade_entregas'], color='salmon')
+for bar in bars_a:
+    width = bar.get_width() # Pega o valor da quantidade
+    plt.text(width + 0.3,  # Posição X (um pouco depois da barra)
+             bar.get_y() + bar.get_height()/2, # Posição Y (no meio da barra)
+             f'{int(width)}', # O texto (a quantidade convertida em inteiro)
+             va='center', fontsize=10, fontweight='bold')
+plt.annotate(f'Total Geral: {total_entregas}', 
+             xy=(0.80, 0.95), xycoords='axes fraction', 
+             fontsize=12, fontweight='bold', color='white',
+             bbox=dict(boxstyle="round,pad=0.5", fc="darkred", ec="black", alpha=0.8))
 plt.title('Quantidade Total de Entregas por Bairro no MES', fontsize=14)
 plt.xlabel('Número de Entregas')
 plt.ylabel('Bairro')
